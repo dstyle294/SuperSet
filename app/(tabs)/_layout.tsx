fb20e5d69f437765436a159627eb618465936bee
Sahil Dalal <skdalal@ucsd.com>
2025-08-10 12:12:29 +0530

    initial tabs setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,67 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import COLORS from '../../constants/colors'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets()
+
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="profile" />
+    <Tabs 
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        headerTitleStyle: {
+          color: COLORS.textPrimary,
+          fontWeight: "600",
+        },
+        headerShadowVisible: false,
+        tabBarStyle: {
+          backgroundColor: COLORS.cardBackground,
+          borderTopWidth: 1,
+          borderTopColor: COLORS.border,
+          paddingTop: 5,
+          paddingBottom: insets.bottom,
+          height: 60 + insets.bottom,
+        }
+      }}
+    >
+      <Tabs.Screen 
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({color, size, focused}) => (<Ionicons name="home-outline" size={size} color={color} />)
+        }} 
+      />
+      <Tabs.Screen 
+        name="workout" 
+        options={{
+          title: "Workout",
+          tabBarIcon: ({color, size, focused}) => (<Ionicons name="barbell-outline" size={size} color={color} />)
+        }} 
+      />
+      <Tabs.Screen 
+        name="post"
+        options={{
+          title: "Post",
+          tabBarIcon: ({color, size, focused}) => (<Ionicons name="add-circle" size={size} color={color} />)
+        }} />
+      <Tabs.Screen 
+        name="exercises" 
+        options={{
+          title: "Exercises",
+          tabBarIcon: ({color, size, focused}) => (<Ionicons name="bicycle-outline" size={size} color={color} />)
+        }} 
+      />
+      <Tabs.Screen 
+        name="profile" 
+        options={{
+          title: "Profile",
+          tabBarIcon: ({color, size, focused}) => (<Ionicons name="person-outline" size={size} color={color} />)
+        }} 
+        />
     </Tabs>
   )
 }
