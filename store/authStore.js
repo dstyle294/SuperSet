80d004e986a6bd829a41085577fb704f07f050fd
Sahil Dalal <skdalal@ucsd.com>
2025-08-13 23:34:23 +0530

    posting mvp working

--- store/authStore.js
+++ store/authStore.js
@@ -1,5 +1,6 @@
 import {create} from "zustand"
 import AsyncStorage from "@react-native-async-storage/async-storage"
+import {API_URL} from '../constants/api'
 
 export const useAuthStore = create((set) => ({
   user: null,
@@ -10,7 +11,7 @@ export const useAuthStore = create((set) => ({
 
     set({ isLoading: true })
     try {
-      const response = await fetch("https://supersetbackend.onrender.com/api/auth/register", {
+      const response = await fetch(`${API_URL}/auth/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -43,7 +44,7 @@ export const useAuthStore = create((set) => ({
   login: async (email, password) => {
     set({ isLoading: true })
     try {
-      const response = await fetch("https://supersetbackend.onrender.com/api/auth/login", {
+      const response = await fetch(`${API_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
