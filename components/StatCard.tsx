8f49018a1bad60bc68ddf0b8e490db938e6c0f3e
Sahil Dalal <skdalal@ucsd.com>
2025-08-20 00:02:02 +0530

    target muscles badges working

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -1,12 +1,12 @@
 import styles from "@/assets/styles/workout.styles"
 import { Text, View } from "react-native"
 
-interface RenderWorkoutProps {
+interface StatCardProps {
   value: string | number,
   label: string,
 }
 
-export const StatCard: React.FC<RenderWorkoutProps> = ({ value, label }) => {
+export const StatCard: React.FC<StatCardProps> = ({ value, label }) => {
   return (
     <View style={styles.statCard}>
       <Text style={styles.statValue}>{value}</Text>
