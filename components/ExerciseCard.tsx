dab677c913e06d9f2334ce4429e926cfcd001789
Sahil Dalal <skdalal@ucsd.com>
2025-08-21 20:37:31 +0530

    looks better

--- components/ExerciseCard.tsx
+++ components/ExerciseCard.tsx
@@ -45,7 +45,7 @@ export const ExerciseCard: React.FC<ExerciseCardProps> = ( { exercise, index } )
             data={exercise.sets}
             keyExtractor={(set) => set._id}
             renderItem={({ item, index }) => (
-              <View style={styles.exerciseDetailText}>
+              <View style={styles.exerciseDetail}>
                 <Text>Set {index + 1}</Text>
                 <Text>Weight {item.weight ?? "-"}</Text>
                 <Text>{item.reps ? `${item.reps} reps` : "-"}</Text>
