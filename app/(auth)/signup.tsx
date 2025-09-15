82fdfac1eee3011447a99d1574479d526ecaf6b9
dstyle294 <skdalal@ucsd.edu>
2025-09-15 23:11:29 +0530

    login page done

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, KeyboardAvoidingView, Platform } from 'react-native'
 import React from 'react'
+import styles from '../../assets/styles/signup.styles'
 
 export default function signup() {
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{flex:1}}
+      behavior={Platform.OS === 'ios' ? "padding" : "height"}
+    >
+      <View style={styles.container}>
+        <View style={styles.card}>
+          {/* HEADER */}
+          
+        </View>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
