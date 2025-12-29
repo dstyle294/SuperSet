0a20cb888f4aec3b6be9e98be7d3f0779c8992a0
dstyle294 <skdalal@ucsd.edu>
2025-12-29 18:57:16 +0100

    profile page header

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -8,12 +8,14 @@ import Loader from '@/components/loader'
 import { DetailRow } from '@/components/DetailRow'
 import { formatMemberSince } from '@/lib/utils'
 import { router, useFocusEffect } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function Profile() {
   const { logout, token } = useAuthStore()
   const [ workoutCount, setWorkoutCount ] = useState(0)
   const [ profile, setProfile ] = useState<profile>()
   const [ loading, setLoading ] = useState(false) 
+  const [ isEditing, setIsEditing ] = useState(false) 
 
   useFocusEffect(
     useCallback(() => {
@@ -61,30 +63,61 @@ export default function Profile() {
 
   return (
     <View style={styles.container}>
-      <DetailRow
-        label="Username"
-        value={profile?.username}
-      />
-      <DetailRow
-        label="Email"
-        value={profile?.email}
-      />
-      <DetailRow
-        label="# of workouts"
-        value={workoutCount}
-      />
-      <DetailRow
-        label="Member since"
-        value={formatMemberSince(profile?.createdAt)}
-      />
-      <TouchableOpacity 
-        onPress={() => {
-          logout()
-          router.replace('/(auth)')
-        }} 
-        style={styles.logoutButton}>
-        <Text>Logout</Text>
-      </TouchableOpacity>
+      <View style={styles.profileHeader}>
+        <Text style={styles.profileTitle}>Profile</Text>
+        <View style={styles.profileIcons}>
+          <TouchableOpacity
+            onPress={() => {
+              setIsEditing(!isEditing)
+            }}
+          >
+            <Ionicons 
+              name="create-outline"
+              style={styles.profileIcon}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={() => {
+              logout()
+              router.replace('/(auth)')
+          }}>
+            <Ionicons 
+              name="log-out-outline"
+              style={styles.profileIcon}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      
     </View>
   )
+
+  // return (
+  //   <View style={styles.container}>
+  //     <DetailRow
+  //       label="Username"
+  //       value={profile?.username}
+  //     />
+  //     <DetailRow
+  //       label="Email"
+  //       value={profile?.email}
+  //     />
+  //     <DetailRow
+  //       label="# of workouts"
+  //       value={workoutCount}
+  //     />
+  //     <DetailRow
+  //       label="Member since"
+  //       value={formatMemberSince(profile?.createdAt)}
+  //     />
+  //     <TouchableOpacity 
+  //       onPress={() => {
+  //         logout()
+  //         router.replace('/(auth)')
+  //       }} 
+  //       style={styles.logoutButton}>
+  //       <Text>Logout</Text>
+  //     </TouchableOpacity>
+  //   </View>
+  // )
 }
